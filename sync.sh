2a442ee58d523e532e9debdc8507897ff822567b
Jacob <jacobmiller0@protonmail.ch>
2026-05-09 03:59:21 +1200

    forced the github push as its a lesser repo

--- sync.sh
+++ sync.sh
@@ -8,6 +8,6 @@ while IFS= read -r sync_path; do
     remote_hash=$(git ls-remote "$sync_path" HEAD | cut -f1)
     
     if [ "$cur_hash" != "$remote_hash" ]; then
-        git push "$sync_path" main
+        git push --force "$sync_path" main
     fi
 done < ".sync/sync_locs"
