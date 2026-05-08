216401fe1a093776894814c3dab8f2afb7468de6
Jacob <jacobmiller0@protonmail.ch>
2026-05-09 04:08:32 +1200

    made sync less brittle

--- sync.sh
+++ sync.sh
@@ -5,8 +5,8 @@ set -e
 cur_hash=$(git rev-parse HEAD)
 
 while IFS= read -r sync_path; do
-    remote_hash=$(git ls-remote "$sync_path" HEAD | cut -f1)
-    
+   remote_hash=$(git ls-remote "$sync_path" HEAD 2>/dev/null | cut -f1 || echo "")
+
     if [ "$cur_hash" != "$remote_hash" ]; then
         git push --force "$sync_path" main
     fi
