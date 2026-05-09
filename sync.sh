60faa27d3c0fa9f90bd2e06a6322cffe80238239
Jacob <jacobmiller0@protonmail.ch>
2026-05-09 18:14:11 +1200

    cleaned up formating

--- sync.sh
+++ sync.sh
@@ -5,9 +5,11 @@ set -e
 cur_hash=$(git rev-parse HEAD)
 
 while IFS= read -r sync_path; do
+
    remote_hash=$(git ls-remote "$sync_path" HEAD 2>/dev/null | cut -f1 || echo "")
 
-    if [ "$cur_hash" != "$remote_hash" ]; then
+   if [ "$cur_hash" != "$remote_hash" ]; then
         git push --force "$sync_path" main
-    fi
-done < ".sync/sync_locs"
+   fi
+
+  done < ".sync/sync_locs"
